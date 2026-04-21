5d9051e10c5a00466a4fcb80da2e571e48e813e2
Oikawa <yucchi0424@gmail.com>
2026-04-21 13:04:48 +0900

    [add] プッシャーに合わせたコイン物理のcsを追加

---
+++ Assets/Scripts/CoinPhysics.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public sealed class CoinPhysics : MonoBehaviour
+{
+
+    private static readonly string TAG_PUSHER = "Pusher";
+    private static readonly string TAG_FINISH = "Finish";
+
+    private bool _isTrigger;
+
+
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(TAG_PUSHER) && _isTrigger)
+        {
+            transform.SetParent(collision.transform, true);
+        }
+        if (collision.gameObject.CompareTag(TAG_FINISH))
+        {
+            CoinMessage.Counter.ChangeCoin(1);
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(TAG_PUSHER))
+        {
+            transform.SetParent(null, true);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(TAG_PUSHER))
+        {
+            _isTrigger = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(TAG_PUSHER))
+        {
+            _isTrigger = false;
+        }
+    }
+}
