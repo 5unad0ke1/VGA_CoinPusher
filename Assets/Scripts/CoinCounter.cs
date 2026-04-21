598831dd142a4bf06d2ebdee3f9724e30577d2be
Oikawa <yucchi0424@gmail.com>
2026-04-21 13:03:40 +0900

    [update] 持ちCoinを扱う.cs追加

---
+++ Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class CoinCounter : MonoBehaviour
+{
+    [SerializeField] private Text _text;
+    [SerializeField] private int _initCoin;
+    [SerializeField] private int _currentCoin;
+
+    public int CurrentCoin => _currentCoin;
+    private void Awake()
+    {
+        _currentCoin = _initCoin;
+        CoinMessage.Counter = this;
+
+        UpdateText();
+    }
+    public void ChangeCoin(int coin)
+    {
+        _currentCoin += coin;
+        UpdateText();
+    }
+    private void UpdateText()
+    {
+        _text.text = _currentCoin.ToString();
+    }
+}
+public static class CoinMessage
+{
+    public static CoinCounter Counter;
+}
